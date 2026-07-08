6f6afb78e59d15a5b368cf25f5186972358e010b
Arinji2 <arinjaydhar205@gmail.com>
2026-07-08 22:13:30 +0530

    Add payment qr codes map

---
+++ src/lib/payment-map.ts
@@ -0,0 +1,20 @@
+import type { StaticImageData } from "next/image";
+import Payment60 from "../../public/payment/60.jpeg";
+import Payment120 from "../../public/payment/120.jpeg";
+
+import Payment180 from "../../public/payment/180.jpeg";
+import Payment200 from "../../public/payment/200.jpeg";
+
+import Payment250 from "../../public/payment/250.jpeg";
+import Payment300 from "../../public/payment/300.jpeg";
+
+import Payment350 from "../../public/payment/350.jpeg";
+export const PaymentMap: Record<string, StaticImageData> = {
+  60: Payment60,
+  120: Payment120,
+  180: Payment180,
+  200: Payment200,
+  250: Payment250,
+  300: Payment300,
+  350: Payment350,
+};
