f73dcb6431033269aae26aca983ad425d8288280
Arinji2 <arinjaydhar205@gmail.com>
2026-07-08 22:12:57 +0530

    Update next config to allow bigger server action body

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-    ],
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
